ccc3f51627fd353bd42122fb6d9443d99872cc5d
Charles Ahn <ahn.charles@wustl.edu>
2025-09-03 15:45:43 -0500

    Studio 1 finished

---
+++ src/studio1/Average.java
@@ -0,0 +1,15 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("value of n1");
+		int n1 = in.nextInt();
+        System.out.println("Value for n2");
+		int n2 = in.nextInt();
+        Average of n1
+    }
+
+}
